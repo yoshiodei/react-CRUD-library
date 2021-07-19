fc8c802a9c96aac5c4abb41435e7547670f10d77
yoshiodei <yoshiodei@yahoo.com>
2021-07-19 16:22:50 +0000

    add Form component

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+                        name: "",
+                        author: "",
+                        genre: "Drama"
+                     }
+    }
+
+    handleChange = (e) => {
+        e.preventDefault();
+        this.setState({[e.target.name]: e.target.value});
+        console.log(this.state);
+    }
+
+    handleSubmit = (e) =>{
+        e.preventDefault();
+        this.props.addBook(this.state);
+        this.setState({
+            name: "",
+            author: "",
+            genre: "Drama"
+        })
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <label>Book Name</label>
-                <input type="text" name="name" placeholder="Please enter book name" />
+                <input type="text" name="name" placeholder="Please enter book name" onChange={this.handleChange} value={this.state.name}/>
                 <label>Author</label>
-                <input type="text" name="author" placeholder="Please enter author name" />
+                <input type="text" name="author" placeholder="Please enter author name" onChange={this.handleChange} value={this.state.author} />
                 <label>Genre</label>
-                <select name="genre">
+                <select name="genre" onChange={this.handleChange} value={this.state.genre}>
                     <option value="Drama">Drama</option>
                     <option value="Mystery">Mystery</option>
                     <option value="Fiction">Fiction</option>
