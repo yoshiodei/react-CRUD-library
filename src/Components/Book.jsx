39d325e93864776b2843581eb643ca5b1977faee
yoshiodei <yoshiodei@yahoo.com>
2021-07-22 00:21:26 +0000

    add Book Component

--- src/Components/Book.jsx
+++ src/Components/Book.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import EditForm from './EditForm';
 
-const Book = ({book}) => {
+const Book = ({book, removeBook, editBook}) => {
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    
+
+    const handleRemove = (e) => {
+        e.preventDefault();
+        removeBook(book.id);
+    }
+
     return (
-        <li key={book.id} className="book">
-            <h5>Yoshi's Library</h5>
-            <p>Book Name: {book.name}</p>
-            <p>Author: {book.author}</p>
-            <p>Genre: {book.genre}</p>
-            <div>
-                <button className="edit">Edit</button>
-                <button className="remove">Remove</button>
-            </div>
-            <hr />
-        </li>
+        <>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <EditForm book={book} editBook={editBook} handleClose={handleClose}/>
+                </Modal.Body>
+            </Modal>
+            
+            <li key={book.id} className="book">
+                <h5>Yoshi's Library</h5>
+                <p>Book Name: {book.name}</p>
+                <p>Author: {book.author}</p>
+                <p>Genre: {book.genre}</p>
+                <div>
+                    <button className="edit" onClick={handleShow}>Edit</button>
+                    <button className="remove" onClick={handleRemove} >Remove</button>
+                </div>
+                <hr />
+            </li>
+        </>
     );
 }
 
